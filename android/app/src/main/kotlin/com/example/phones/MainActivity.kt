ab2ece223924cc2334afc7396d8d810341af9d7a
kimoo112 <kimo8123008@gmail.com>
2023-05-26 17:12:58 +0200

    Phones

---
+++ android/app/src/main/kotlin/com/example/phones/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.phones
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/data/phones_data.dart
@@ -0,0 +1,55 @@
+List<Map> phones = [
+  {
+    'name': 'Xiaomi Redmi Note 9',
+    'price': 148,
+    'categories': "0\$ - 149\$",
+    'phoneCategories': 'Android 12',
+    'ram': '6GB',
+    'processor': 'MediaTek',
+    'wifi': '4G',
+    'rating': 5.0,
+    'phoneImage': 'assets/phone1.png'
+  },
+  {
+    'name': 'Apple iPhone SE 2020',
+    'price': 399,
+    'categories': '150\$ - 499\$',
+    'phoneCategories': 'iOS 15',
+    'ram': '3GB',
+    'processor': 'A13 Bionic',
+    'wifi': '4G',
+    'rating': 3.5,
+    'phoneImage': 'assets/phone2.png'
+  },
+  {
+    'name': 'Oppo Reno 6 Pro 5G',
+    'price': 780,
+    'categories': '500\$ - 1500\$',
+    'phoneCategories': 'Android 12',
+    'ram': '12GB',
+    'processor': 'Snapdragon',
+    'wifi': '5G',
+    'rating': 2.5,
+    'phoneImage': 'assets/oppo.png'
+  },
+  {
+    'name': 'Apple iPhone 13 Pro Max',
+    'price': 1099,
+    'phoneCategories': 'iOS 15',
+    'ram': '6GB',
+    'processor': 'A15 Bionic',
+    'wifi': '5G',
+    'rating': 4.0,
+    'phoneImage': 'assets/phone3.png'
+  },
+  {
+    'name': 'Sony Xperia 1 III',
+    'price': 1300,
+    'phoneCategories': 'Android 12',
+    'ram': '12GB',
+    'processor': 'Snapdragon',
+    'wifi': '4G-5G',
+    'rating': 3.5,
+    'phoneImage': 'assets/sony.png'
+  },
+];
